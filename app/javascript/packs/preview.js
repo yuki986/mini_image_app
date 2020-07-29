eea3381eeb15f94f91d5ca3b178c22152376f920
kokkedx <kokkedx2008@gmail.com>
2020-07-29 11:10:48 +0900

    inputのなかにある画像を取得しましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
+  const ImageList = document.getElementById('image-list')
   document.getElementById('message_image').addEventListener('change', function(e){
-    console.log(123);
+    console.log(e);
   });
 });
