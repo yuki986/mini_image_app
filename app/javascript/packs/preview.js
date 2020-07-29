b6484dd9297539566ae3569257dd087095108e31
kokkedx <kokkedx2008@gmail.com>
2020-07-29 11:27:53 +0900

    TOP画面に表示されているエラーを解決しましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,19 +1,21 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const ImageList = document.getElementById('image-list')
+if (window.location.pathname === '/messages/new') {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list')
 
-  document.getElementById('message_image').addEventListener('change', function(e){
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
+    document.getElementById('message_image').addEventListener('change', function(e){
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
 
-    // 画像を表示するためのdiv要素を生成
-    const imageElement = document.createElement('div')
+      // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div')
 
-    // 表示する画像を生成
-    const blobImage = document.createElement('img')
-    blobImage.setAttribute('src', blob)
+      // 表示する画像を生成
+      const blobImage = document.createElement('img')
+      blobImage.setAttribute('src', blob)
 
-    // 生成したHTMLの要素をブラウザに表示させる
-    imageElement.appendChild(blobImage)
-    ImageList.appendChild(imageElement)
+      // 生成したHTMLの要素をブラウザに表示させる
+      imageElement.appendChild(blobImage)
+      ImageList.appendChild(imageElement)
+    });
   });
-});
+}
