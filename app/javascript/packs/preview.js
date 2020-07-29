66a8595ac852ff11e6d06642b3b3d5d695895cd8
kokkedx <kokkedx2008@gmail.com>
2020-07-29 11:19:25 +0900

    画像情報を変数に格納しましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list')
   document.getElementById('message_image').addEventListener('change', function(e){
-    console.log(e);
+    const file = e.target.files[0];
   });
 });
