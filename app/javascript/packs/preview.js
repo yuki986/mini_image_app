4bc879346bcd4b4522e678e9c6aae7d37611febf
kokkedx <kokkedx2008@gmail.com>
2020-07-29 11:04:20 +0900

    input要素で値の変化が起こったときに呼び出される関数を定義しましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-  const ImageList = document.getElementById('image-list')
+  document.getElementById('message_image').addEventListener('change', function(e){
+    console.log(123);
+  });
 });
