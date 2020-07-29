87acc0217eceb91758b5622326b3d7589bbccb56
kokkedx <kokkedx2008@gmail.com>
2020-07-29 11:22:03 +0900

    取得した画像情報のURLを生成しましょう。

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -2,5 +2,6 @@ document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list')
   document.getElementById('message_image').addEventListener('change', function(e){
     const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
   });
 });
