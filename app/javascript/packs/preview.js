a2ce2532d4602a2bfe507d7525f77344d432f7da
kokkedx <kokkedx2008@gmail.com>
2020-07-29 11:30:15 +0900

    表示されている画像を編集可能にしましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -3,6 +3,11 @@ if (window.location.pathname === '/messages/new') {
     const ImageList = document.getElementById('image-list')
 
     document.getElementById('message_image').addEventListener('change', function(e){
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      };
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
