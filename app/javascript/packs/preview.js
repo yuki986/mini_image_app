a09b5a2b2b2e1bd613aa3ebb26de3f670248ce14
kokkedx <kokkedx2008@gmail.com>
2020-07-29 11:25:01 +0900

    画像のHTMLを作成しましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list')
+
   document.getElementById('message_image').addEventListener('change', function(e){
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
+
+    // 画像を表示するためのdiv要素を生成
+    const imageElement = document.createElement('div')
+    
+    // 表示する画像を生成
+    const blobImage = document.createElement('img')
   });
 });
