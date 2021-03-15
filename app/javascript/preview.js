a4268f8ab1d70f056f099c9ee92b5dc1b28ede44
yuki986 <yuki.kuribayashi8@gmail.com>
2021-03-15 14:40:57 +0900

    imageを動かすためにjsを書いた。

---
+++ app/javascript/preview.js
@@ -0,0 +1,26 @@
+if (document.URL.match(/new/) || document.URL.match(/edit/)) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
+  
+    document.getElementById('message_image').addEventListener('change', function(e){
+      // 画像が表示されてる場合のみ、既に存在する画像を削除する。
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+  
+      // 画像生成のためのdiv要素を生成
+      const imageElement = document.createElement('div');
+  
+      // 表示する画像生成
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+
+      // 生成したHTMLの要素をブラウザに表示させる。
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    });
+  });
+}
